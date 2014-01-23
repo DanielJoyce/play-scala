7a9d0d6d5a5d37107b931da09e3080994bcf46bc
Daniel A Joyce <daniel@edifice-technologies.com>
2014-01-23 12:50:44 -0800

    First stab at new api

--- src-compiler/PlayScalaZincCompiler.scala
+++ src-compiler/PlayScalaZincCompiler.scala
@@ -4,11 +4,21 @@ import com.typesafe.config._
 import com.typesafe.zinc._
 import java.io.File
 import xsbti.compile.CompileOrder
+import sbt.inc.Analysis
+
 
 class PlayScalaZincCompiler(app: File, libs: File, classpath: List[File], output: File, skipResult: Boolean = false) {
-
+    
     def update(sources: List[File]): Either[CompilationError, (List[ClassDefinition], List[ClassDefinition])] = {
+        
+    		
+        
+        
         return Right(Nil, Nil)
     }
 
+}
+
+object PlayScalaZincCompiler{
+    
 }
